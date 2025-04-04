222d74f6cd01b4c1b25eb3da7684f2c4770e2097
jiwon <kjw042626@gmail.com>
2025-04-04 21:00:33 +0900

    View 환경설정

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-
-		Hello hello = new Hello();
-		hello.setData("Hello World");
-		String data = hello.getData();
-		System.out.println("data = " + data);
-		Hello hello2 = new Hello();
-		hello2.setData("안녕하세요");
-		String data2 = hello2.getData();
-		System.out.println("data2 = " + data2);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
