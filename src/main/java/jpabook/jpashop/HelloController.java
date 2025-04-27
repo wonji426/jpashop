2dbdc18fb78bd0b6493bb50f8a9fa7b5a83332b6
jiwonKang <kjw042626@gmail.com>
2025-04-27 21:33:03 +0900

    HelloController.java 업데이트

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model) {
+    public String hello(Model model) {//모델에 데이터 넘기기
         model.addAttribute("data", "Hello!!!");
         return "hello";
     }
